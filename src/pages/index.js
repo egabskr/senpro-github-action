75128cd380d370a1822069397367c64b26140a50
egabskr <egabnsoswet@gmail.com>
2026-02-24 10:43:20 +0700

    Edit main.yaml case 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            Ega Baskara Nugroho | 521518
+            [Nama] | [NIU]
           </h5>
         </div>
       </main>
